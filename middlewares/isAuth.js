5f19661510e4e3681bcb2c968065da3a6702d8cb
Manoj kumar Das <dasmanojkumar308@gmail.com>
2026-04-02 15:07:08 +0530

    hii

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,20 +1,21 @@
- import jwt from "jsonwebtoken"
- const isAuth=async (req,res,next)=>{
-    try {
-        const token=req.cookies.token
-        if(!token){
-            return res.status(400).json({message:"token is not found"})
-        }
+import jwt from "jsonwebtoken";
 
-   const verifyToken=await jwt.verify(token,process.env.JWT_SECRET)  
-   
-   req.userId=verifyToken.userId
+const isAuth = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
 
-   next()
-
-    } catch (error) {
-        return res.status(500).json({message:`is auth error ${error}`})
+    if (!token) {
+      return res.status(400).json({ message: "token is not found" });
     }
- }
 
- export default isAuth
+    const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = verifyToken.userId;
+
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: `is auth error ${error}` });
+  }
+};
+
+export default isAuth;
